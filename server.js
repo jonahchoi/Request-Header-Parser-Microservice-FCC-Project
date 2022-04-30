478cb88ca5bff63285bc1250411bd4bdfa6a6e6a
Glitch (resisted-ionized-guppy) <none>
2022-04-30 03:04:51 +0000

    🌵🌔 Checkpoint ./server.js:44333664/324

--- server.js
+++ server.js
@@ -21,8 +21,14 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  let ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+  
+  res.json({
+     ipadress: ip, 
+  //   language: '', 
+  //   software: ''
+  });
 });
 
 
